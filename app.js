1cca8d5e8746c5f5eff3f7f064d2f51d961576c1
eric9530873 <eric9530873@gmail.com>
2023-03-03 17:49:05 +0800

    3/3

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require('express')
 const mongoose = require('mongoose')
 
+const exhbs = require('express-handlebars')
+
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
 }
 
 const app = express()
-mongoose.connect(process.env.MONGODB_URI) // 設定連線到 mongoDB
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
 
-const db = mongoose.connect
+const db = mongoose.connection
 
 db.on('error', () => {
     console.log('mongodb error')
@@ -18,9 +20,12 @@ db.once('open', () => {
     console.log('mongodb connected')
 })
 
+app.engine('hbs', exhbs.engine({ defaultLayout: 'main', extname: 'hbs' }))
+
+app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
-    res.send('hello world')
+    res.render('index')
 })
 
 app.listen(3000, () => {
