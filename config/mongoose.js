3f2c27f7e8c5fc1732481f91ed051e56fe7f9e5f
eric9530873 <eric9530873@gmail.com>
2023-03-08 13:03:17 +0800

    3/8

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose')
 
-
-// 加入這段 code, 僅在非正式環境時, 使用 dotenv
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
-}
+require('dotenv').config()
+// if (process.env.NODE_ENV === 'production') {
+//     require('dotenv').config()
+// }
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
 
