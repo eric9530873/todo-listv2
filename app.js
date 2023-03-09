e001da3a8ccc9679da75b9a0e11ecfdf13dbcf31
eric9530873 <eric9530873@gmail.com>
2023-03-09 18:20:37 +0800

    3/9

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express')
-
+const session = require('express-session')
 const exhbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const usePassport = require('./config/passport')
 
 require('./config/mongoose')
 
@@ -15,11 +16,17 @@ const PORT = process.env.PORT || 3000
 
 
 app.engine('hbs', exhbs.engine({ defaultLayout: 'main', extname: 'hbs' }))
-
 app.set('view engine', 'hbs')
-app.use(bodyParser.urlencoded({ extended: true }))
 
+app.use(session({
+    secret: 'ThisIsMySecret',
+    resave: false,
+    saveUninitialized: true
+}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+usePassport(app)
 
 app.use(routes)
 
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const router = express.Router()
 
+const passport = require('passport')
+
 const User = require('../../models/user')
 
 router.get('/login', (req, res) => {
     res.render('login')
 })
+
+router.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/users/login'
+}));
 
 router.get('/register', (req, res) => {
     res.render('register')
@@ -46,6 +53,11 @@ router.post('/register', (req, res) => {
             }
         })
         .catch(err => console.log(err))
+})
+
+router.get('/logout', (req, res) => {
+    req.logout()
+    res.redirect('/users/login')
 })
 
 module.exports = router
